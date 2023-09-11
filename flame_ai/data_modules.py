b3d1a167b9b695fadc326358358f8aae966724f9
PradG <pradeep.ganesan@gmail.com>
2023-09-11 10:26:42 -0500

    Updated data modules

--- flame_ai/data_modules.py
+++ flame_ai/data_modules.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-import torch
 from torch.utils.data import Dataset
+from torchvision.transforms import Normalize, Compose, ToTensor
 
 
 class FlowFieldDataset(Dataset):
@@ -16,9 +16,10 @@ class FlowFieldDataset(Dataset):
 
         self.mean = np.array([0.24, 28.0, 28.0, 28.0])
         self.std = np.array([0.068, 48.0, 48.0, 48.0])
+        self.normalize = Normalize(self.mean, self.std, inplace=True)
 
-    def normalize(self, x):
-        return (x - self.mean) / self.std
+    def transform(self, x):
+        return Compose([ToTensor(), self.normalize])(x)
 
     def __len__(self):
         return len(self.csv_file)
@@ -32,9 +33,7 @@ class FlowFieldDataset(Dataset):
             uy_i = np.fromfile(self.LR_path + "/" + self.csv_file["uy_filename"][idx], dtype="<f4").reshape(16, 16)
             uz_i = np.fromfile(self.LR_path + "/" + self.csv_file["uz_filename"][idx], dtype="<f4").reshape(16, 16)
             X = np.stack([rho_i, ux_i, uy_i, uz_i], axis=2)
-            X = self.normalize(X)
-            # X = (X - X.min()) / (X.max() - X.min())
-            return id, torch.tensor(X).movedim((0, 2), (2, 0)).type(torch.float32)
+            return id, self.transform(X)
 
         rho_i = np.fromfile(self.LR_path + "/" + self.csv_file["rho_filename"][idx], dtype="<f4").reshape(16, 16)
         ux_i = np.fromfile(self.LR_path + "/" + self.csv_file["ux_filename"][idx], dtype="<f4").reshape(16, 16)
@@ -47,13 +46,4 @@ class FlowFieldDataset(Dataset):
         uz_o = np.fromfile(self.HR_path + "/" + self.csv_file["uz_filename"][idx], dtype="<f4").reshape(128, 128)
         X = np.stack([rho_i, ux_i, uy_i, uz_i], axis=2)
         Y = np.stack([rho_o, ux_o, uy_o, uz_o], axis=2)
-        # [(X[:, :, i].min(), X[:, :, i].max()) for i in range(X.shape[-1])]
-        # X = (X - X.min()) / (X.max() - X.min())
-        # Y = (Y - Y.min()) / (Y.max() - Y.min())
-        # X = (X - X.min(axis=(0, 1))) / (X.max(axis=(0, 1)) - X.min(axis=(0, 1)))
-        # Y = (Y - Y.min(axis=(0, 1))) / (Y.max(axis=(0, 1)) - Y.min(axis=(0, 1)))
-        X = self.normalize(X)
-        Y = self.normalize(Y)
-        X = torch.tensor(X).movedim((0, 2), (2, 0)).type(torch.float32)
-        Y = torch.tensor(Y).movedim((0, 2), (2, 0)).type(torch.float32)
-        return X, Y
+        return self.transform(X), self.transform(Y)
